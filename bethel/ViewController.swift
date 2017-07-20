08e81750b3124c9ac3376dd376717cce0a6a11a7
Chris Petersen <chris@detroitlabs.com>
2017-07-20 11:20:13 -0400

    Initial Commit

---
+++ bethel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bethel
+//
+//  Created by Chris Petersen on 7/20/17.
+//  Copyright © 2017 Detroit Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
